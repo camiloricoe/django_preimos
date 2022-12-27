178794d281f6cc624223a753d1df23cd8c5a0992
camiloricoe <camilo.rico.echeverry@gmail.com>
2022-12-26 20:02:01 -0500

    URL + View + Templates

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello World")
+from .models import Question
 
 # Create your views here.
+def index(request):
+    latest_question_list = Question.objects.all()
+    return render(request,"polls/index.html",{
+        "latest_question_list": latest_question_list
+    })
+
+
+def detail(request, question_id):
+    #question = Question.objects.get(pk=question_id) 
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, 'polls/detail.html', {
+        "question":question
+    })
+
+
+def results(request, question_id):
+    return HttpResponse(f'Estas viendo los votos de la pregunta {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'Estas votando la pregunta {question_id}')
